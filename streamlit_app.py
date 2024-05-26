82d58779252f7d6eace7ee52b3f121bdcfdadf8e
bvorak <bvorak@users.noreply.github.com>
2024-05-26 21:37:30 +0000

    asdfa

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,14 +32,14 @@ df_reshaped = pd.read_csv('data/us-population-2010-2019-reshaped.csv')
 with st.sidebar:
     #st.title('🏂 US Population Dashboard')
     
-    #year_list = list(df_reshaped.year.unique())[::-1]
+    year_list = list(df_reshaped.year.unique())[::-1]
     
-    #selected_year = st.selectbox('Select a year', year_list)
-    #df_selected_year = df_reshaped[df_reshaped.year == selected_year]
-    #df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
+    selected_year = st.selectbox('Select a year', year_list)
+    df_selected_year = df_reshaped[df_reshaped.year == selected_year]
+    df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
 
-    #color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
-    #selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
+    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
 
     st.title("🐙 Streamlit-tree-select")
     st.subheader("A simple and elegant checkbox tree for Streamlit.")
